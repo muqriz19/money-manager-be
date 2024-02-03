e4a53b64600c791b6528996fcda6a5b5784e5cf5
muqriz19 <muqriz19othman@gmail.com>
2024-02-03 14:44:45 +0800

    record description optional

--- Models/Record.cs
+++ Models/Record.cs
@@ -7,7 +7,7 @@ namespace moneyManagerBE.Models
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
         public DateTimeOffset CreatedDate { get; set; } = new DateTimeOffset();
         public int AccountId { get; set; }
         public Collection<Log>? Logs { get; set; }
