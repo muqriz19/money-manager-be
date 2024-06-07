19889986479ba277b55d86aba02c99fd1b73ccdf
muqriz19 <muqriz19othman@gmail.com>
2024-06-07 17:38:32 +0800

    init

--- obj/Debug/net8.0/moneyManagerBE.AssemblyInfo.cs
+++ obj/Debug/net8.0/moneyManagerBE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moneyManagerBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff99ede350fd7e0fe08d4cfc1dbb442024f8d4a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f35f18ff41b06209f48aa685488a8650794a107")]
 [assembly: System.Reflection.AssemblyProductAttribute("moneyManagerBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moneyManagerBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
