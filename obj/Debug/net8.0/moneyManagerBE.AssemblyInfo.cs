eeab8858a988bb600ac1557022de18cfd648aee1
muqriz19 <muqriz19othman@gmail.com>
2024-08-02 17:44:47 +0800

    commit

--- obj/Debug/net8.0/moneyManagerBE.AssemblyInfo.cs
+++ obj/Debug/net8.0/moneyManagerBE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moneyManagerBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+416c330187a7c738831e0c63bd84fc48c17cc8a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d990fc561783dcc2003b87e4fffccf6cd1102b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("moneyManagerBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moneyManagerBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
